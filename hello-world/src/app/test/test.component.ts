9b38d66f9437a7271f830571cfa455abf9aa3ede
Abdulrehman Saleem <bsef20m033@pucit.edu.pk>
2024-07-09 13:38:30 +0500

    Created test and renders inside app and test2 is rendered inside test

---
+++ hello-world/src/app/test/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Test2Component } from '../test2/test2.component';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [Test2Component],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+  title = 'Abdulrehman testing'
+}
