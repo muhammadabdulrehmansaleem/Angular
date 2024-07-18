412241ff3c5ff1b1782c0d615a3cc7928b6ccea7
Abdulrehman Saleem <bsef20m033@pucit.edu.pk>
2024-07-18 17:27:40 +0500

    child routing optional param and more about routing

--- hello-world/src/app/employee-list/employee-list.component.ts
+++ hello-world/src/app/employee-list/employee-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IEmployee } from '../Employee.model';
 import { EmployeeService } from '../employee.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-employee-list',
   standalone: true,
@@ -10,9 +11,22 @@ import { EmployeeService } from '../employee.service';
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  public empId = 0;
   employees: IEmployee[] = [];
-  constructor(private _employeeservice: EmployeeService) { }
+  constructor(private _employeeservice: EmployeeService, private router: Router, private route: ActivatedRoute) { }
   ngOnInit() {
     this._employeeservice.getEmployee().subscribe(data => this.employees = data, error => console.log(error));
+    this.route.paramMap.subscribe((param: ParamMap) => {
+      let te = param.get('id');
+      if (te != null) {
+        this.empId = parseInt(te);
+      }
+    })
+  }
+  onSelect(employee: any) {
+    this.router.navigate(['/employee', employee.id]);
+  }
+  isSelected(employee: IEmployee) {
+    return this.empId === employee.id;
   }
 }
